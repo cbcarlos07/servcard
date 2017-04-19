538ef8fd3b966d9ea70ffcc62fa17ea5eda7220a
cbcarlos07 <cbcarlos08@gmail.com>
2017-04-18 20:14:09 -0400

    Botão voltar

--- model/ConnectionFactory.class.php
+++ model/ConnectionFactory.class.php
@@ -8,9 +8,9 @@
  */
 class ConnectionFactory extends PDO
 {
-    private $dsn = 'mysql:host=localhost;dbname=servc617_servcard';
+    private $dsn = 'mysql:host=localhost;dbname=servcard';
 
-    private $user = 'servc617_servcar';
+    private $user = 'servcard';
     private $password = 'servcard123';
     public $handle = null;
 
